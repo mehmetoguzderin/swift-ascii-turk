7e5770863e0963bada1d2f2d49b6bb6025783cec
Mehmet Oguz Derin <mehmetoguzderin@mehmetoguzderin.com>
2022-01-13 00:31:41 +0300

    Update AsciiTurk.swift

--- Sources/AsciiTurk/AsciiTurk.swift
+++ Sources/AsciiTurk/AsciiTurk.swift
@@ -135,9 +135,9 @@ let asciiTurk = [
         front: "\u{10c07}",
         backness: .front,
         implicit: false),
-    "\u{00062}": Letter(
-        back: "\u{10c09}",
-        front: "\u{10c0b}",
+    "\u{0006b}": Letter(
+        back: "\u{10c34}",
+        front: "\u{10c1a}",
         backness: nil,
         implicit: false),
     "\u{00067}": Letter(
@@ -145,49 +145,19 @@ let asciiTurk = [
         front: "\u{10c0f}",
         backness: nil,
         implicit: false),
+    "\u{00063}": Letter(
+        back: "\u{10c32}",
+        front: "\u{10c32}",
+        backness: nil,
+        implicit: false),
+    "\u{00074}": Letter(
+        back: "\u{10c43}",
+        front: "\u{10c45}",
+        backness: nil,
+        implicit: false),
     "\u{00064}": Letter(
         back: "\u{10c11}",
         front: "\u{10c13}",
-        backness: nil,
-        implicit: false),
-    "\u{0007a}": Letter(
-        back: "\u{10c14}",
-        front: "\u{10c14}",
-        backness: nil,
-        implicit: false),
-    "\u{00079}": Letter(
-        back: "\u{10c16}",
-        front: "\u{10c18}",
-        backness: nil,
-        implicit: false),
-    "\u{0006b}": Letter(
-        back: "\u{10c34}",
-        front: "\u{10c1a}",
-        backness: nil,
-        implicit: false),
-    "\u{0006c}": Letter(
-        back: "\u{10c1e}",
-        front: "\u{10c20}",
-        backness: nil,
-        implicit: false),
-    "\u{0006d}": Letter(
-        back: "\u{10c22}",
-        front: "\u{10c22}",
-        backness: nil,
-        implicit: false),
-    "\u{0006e}": Letter(
-        back: "\u{10c23}",
-        front: "\u{10c24}",
-        backness: nil,
-        implicit: false),
-    "\u{0006a}": Letter(
-        back: "\u{10c2a}",
-        front: "\u{10c2a}",
-        backness: nil,
-        implicit: false),
-    "\u{00071}": Letter(
-        back: "\u{10c2c}",
-        front: "\u{10c2d}",
         backness: nil,
         implicit: false),
     "\u{00070}": Letter(
@@ -195,19 +165,9 @@ let asciiTurk = [
         front: "\u{10c2f}",
         backness: nil,
         implicit: false),
-    "\u{00063}": Letter(
-        back: "\u{10c32}",
-        front: "\u{10c32}",
-        backness: nil,
-        implicit: false),
-    "\u{00072}": Letter(
-        back: "\u{10c3a}",
-        front: "\u{10c3c}",
-        backness: nil,
-        implicit: false),
-    "\u{00073}": Letter(
-        back: "\u{10c3d}",
-        front: "\u{10c3e}",
+    "\u{00062}": Letter(
+        back: "\u{10c09}",
+        front: "\u{10c0b}",
         backness: nil,
         implicit: false),
     "\u{00078}": Letter(
@@ -215,15 +175,55 @@ let asciiTurk = [
         front: "\u{10c40}",
         backness: nil,
         implicit: false),
-    "\u{00074}": Letter(
-        back: "\u{10c43}",
-        front: "\u{10c45}",
+    "\u{00073}": Letter(
+        back: "\u{10c3d}",
+        front: "\u{10c3e}",
         backness: nil,
-        implicit: false)
+        implicit: false),
+    "\u{0007a}": Letter(
+        back: "\u{10c14}",
+        front: "\u{10c14}",
+        backness: nil,
+        implicit: false),
+    "\u{00071}": Letter(
+        back: "\u{10c2c}",
+        front: "\u{10c2d}",
+        backness: nil,
+        implicit: false),
+    "\u{0006a}": Letter(
+        back: "\u{10c2a}",
+        front: "\u{10c2a}",
+        backness: nil,
+        implicit: false),
+    "\u{0006e}": Letter(
+        back: "\u{10c23}",
+        front: "\u{10c24}",
+        backness: nil,
+        implicit: false),
+    "\u{0006d}": Letter(
+        back: "\u{10c22}",
+        front: "\u{10c22}",
+        backness: nil,
+        implicit: false),
+    "\u{00072}": Letter(
+        back: "\u{10c3a}",
+        front: "\u{10c3c}",
+        backness: nil,
+        implicit: false),
+    "\u{00079}": Letter(
+        back: "\u{10c16}",
+        front: "\u{10c18}",
+        backness: nil,
+        implicit: false),
+    "\u{0006c}": Letter(
+        back: "\u{10c1e}",
+        front: "\u{10c20}",
+        backness: nil,
+        implicit: false),
 ]
 
 let asciiFricativeAsciiOcclusive = [
-    "\u{00076}": "\u{00062}",
     "\u{00068}": "\u{0006b}",
     "\u{00066}": "\u{00070}",
+    "\u{00076}": "\u{00062}",
 ]
